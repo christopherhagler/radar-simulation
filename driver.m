d0649a53a84720753deaf4d81074e1d9347e0e04
Christopher Hagler <haglerchristopher@gmail.com>
2025-12-06 21:46:23 -0600

    More work on simulation

--- driver.m
+++ driver.m
@@ -1,12 +1,17 @@
 clc;
+close all;
 clear variables;
 
+% Target's range and velocity
+targetParams.targetState = [50e3, 200];
+
+% Simple monostatic planar array
 radarParams.fc = 450e6;
 radarParams.Pt = 10e3;
 radarParams.Gt = 630;
 radarParams.Gr = 630;
-radarParams.Rt = 150e3;
-radarParams.Rr = 150e3;
+radarParams.Rt = targetParams.targetState(:,1);
+radarParams.Rr = targetParams.targetState(:,1);
 radarParams.RCS = 10;
 radarParams.T = 290;
 radarParams.loss = 2.0;
@@ -15,14 +20,23 @@ radarParams.numChan = 64;
 radarParams.fs = 6e6;
 radarParams.elemenPos_ENU = genPlanarArray(radarParams.numChan, freq2wavelen(radarParams.fc)/2);
 
-waveformParams.tau = 100e-6;     % 100 microseconds
-waveformParams.PRI = 1.5e-3;     % 1.5 milliseconds
-waveformParams.bandwidth = 2e6;  % 2 MHz chirp
-waveformParams.type = 'lfm';     % Linear Frequency Modulation
-waveformParams.numPRI = 4;       % number of PRIs
+waveformParams.tau = 100e-6;
+waveformParams.PRI = 1.5e-3;
+waveformParams.bandwidth = 2e6;
+waveformParams.type = 'lfm';
+waveformParams.numPRI = 4;
 
-% 1). Calculate the recieved signal power for a given target range
-[SNR_dB, ~] = radarRangeEquation(radarParams, waveformParams);
+% Plot the array geometry to confirm setup
+elementSpacing = freq2wavelen(radarParams.fc)/2;
+figure(1);
+X = radarParams.elemenPos_ENU(:,1);
+Y = radarParams.elemenPos_ENU(:,2);
+Z = radarParams.elemenPos_ENU(:,3);
+scatter3(X,Y,Z, 'filled');
+title(sprintf('8x8 Planar Array, Spacing: %2.3fm', elementSpacing));
+
+% 1). Calculate the recieved signal power for a given target
+[SNR_dB, ~] = RREForSNR(radarParams, waveformParams);
 
 % 2). Generate an LFM chirp.
 txSig = genWaveform(radarParams.fs, waveformParams);
--- genPlanarArray.m
+++ genPlanarArray.m
@@ -1,26 +1,12 @@
 function pos_ENU = genPlanarArray(numChan, spacing)
-    % 1. Determine dimensions (assume square array)
     sideLen = sqrt(numChan);
-    
-    % Validation: Ensure it is a perfect square
+
     if mod(sideLen, 1) ~= 0
         error('Error: numChan (%d) is not a perfect square. Cannot create a square grid.', numChan);
     end
 
-    % 2. Create the axis coordinates
-    % We start at 0 so the first element is the phase center reference (0,0,0)
     axis_coords = (0:sideLen-1) * spacing;
-
-    % 3. Generate the 2D Mesh
-    % Grid_X varies along columns (East)
-    % Grid_Y varies along rows (North)
     [Grid_Y, Grid_Z] = meshgrid(axis_coords, axis_coords);
-
-    % 4. Create X coordinates (Flat array = 0)
     Grid_X = zeros(size(Grid_Y));
-
-    % 5. Flatten and arrange into 3xN matrix
-    % result is [x1 x2...; y1 y2...; z1 z2...]
     pos_ENU = [Grid_X(:), Grid_Y(:), Grid_Z(:)];
-
 end
